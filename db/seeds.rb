264d9ecb734aa31797987105060706ef9a1a5b65
jantayreyes <jantayreyes@gmail.com>
2020-09-01 16:56:12 +0200

    10 articles generated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do  
+  article = Article.new
+  article.title = Faker::Book.title 
+  article.content = Faker::TvShows::VentureBros.quote
+end
